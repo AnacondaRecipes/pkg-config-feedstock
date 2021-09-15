447edc5ab20bc028f25925664814d816252403b9
Kai Tietz <ktietz@anaconda.com>
2021-09-15 17:08:02 +0200

    add windows 32 bit support ...

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,22 @@
 @echo ON
 
-mkdir release\
-mkdir release\x64
-mkdir release\x64\pkg-config
+if "%ARCH%" == "64" (
+  mkdir release\
+  mkdir release\x64
+  mkdir release\x64\pkg-config
+
+) else (
+  mkdir release\
+  mkdir release\Win32
+  mkdir release\Win32\pkg-config
+)
 nmake /f Makefile.vc CFG=release GLIB_PREFIX=%LIBRARY_PREFIX%
 if errorlevel 1 exit 1
 
-copy release\x64\pkg-config.exe %LIBRARY_PREFIX%\bin\pkg-config.exe
+if "%ARCH%" == "64" (
+  copy release\x64\pkg-config.exe %LIBRARY_PREFIX%\bin\pkg-config.exe
+) else (
+  copy release\Win32\pkg-config.exe %LIBRARY_PREFIX%\bin\pkg-config.exe
+)
+
 if errorlevel 1 exit 1
